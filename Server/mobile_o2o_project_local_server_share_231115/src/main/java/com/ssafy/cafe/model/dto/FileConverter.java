0f1deae9120e4ceb114104e70a389851a8eaa012
KimKunWoo <rlarjsdn0407@naver.com>
2023-11-17 09:46:24 +0900

    이미지 업로드 고치는중

---
+++ Server/mobile_o2o_project_local_server_share_231115/src/main/java/com/ssafy/cafe/model/dto/FileConverter.java
@@ -0,0 +1,26 @@
+package com.ssafy.cafe.model.dto;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.multipart.MultipartFile;
+
+public class FileConverter {
+
+	public static UploadFile storeFile(MultipartFile file, String folder) throws IOException{
+		if(file.isEmpty()) return null;
+		String originName = file.getOriginalFilename();
+		String storeName = UUID.randomUUID()+"."+extractExt(originName);
+		File saveFile = new File("C:/Users/SSAFY/Desktop/finalProject/finalproject/Server/mobile_o2o_project_local_server_share_231115/src/main/resources/static/imgs"+folder, storeName);
+//		file.transferTo();
+		FileCopyUtils.copy(file.getBytes(), saveFile);
+		return new UploadFile(originName, storeName);
+	}
+	
+	private static String extractExt(String origin) {
+        int pos = origin.lastIndexOf(".");
+        return origin.substring(pos + 1);
+    }
+}
